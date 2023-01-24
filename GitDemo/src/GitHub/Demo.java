6fa71a486f77cc5256e82cc48d5f208a9543feae
AbhayaChavan <123442189+AbhayaChavan@users.noreply.github.com>
2023-01-24 13:00:50 +0530

    Update Demo.java

--- GitDemo/src/GitHub/Demo.java
+++ GitDemo/src/GitHub/Demo.java
@@ -2,8 +2,15 @@ package GitHub;
 
 public class Demo 
 {
+	public void m1()
+	{
+		
+	}
+	
   public static void main(String args[])
   {
 	  System.out.println("Welcome to GitHub");
+	  
+	  System.out.println("Hello GitHub");
   }
 }
